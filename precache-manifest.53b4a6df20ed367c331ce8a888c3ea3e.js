3d740475f21b409a784970ca7b3b0ae0fe0344ba
wemogy Mac 1 <wemogymac@wemogys-MBP.fritz.box>
2020-07-13 13:41:23 +0200

    Updates

---
+++ precache-manifest.53b4a6df20ed367c331ce8a888c3ea3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ead3d03c119c2d912125095bf7c7d5c",
+    "url": "/react-hooks-games/index.html"
+  },
+  {
+    "revision": "1e79b611fd2201784f39",
+    "url": "/react-hooks-games/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "28144619b1ff1b6c3734",
+    "url": "/react-hooks-games/static/js/2.49031cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hooks-games/static/js/2.49031cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e79b611fd2201784f39",
+    "url": "/react-hooks-games/static/js/main.6bff7943.chunk.js"
+  },
+  {
+    "revision": "c1ec8fb5b53c10773549",
+    "url": "/react-hooks-games/static/js/runtime-main.aaf69bc9.js"
+  }
+]);
